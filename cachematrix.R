625d052a8e5b30e8a875987838be3df3bc8d9243
unknown <virgchu@gmail.com>
2015-08-19 19:51:19 -0400

    HWAssignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##vich
+
+## makeCacheMatrix should make be able to cache an inverse of a matrix x, that doesn't have to be square
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invx<-NULL
+  set <-function (v) {
+    x<<-y
+    invx<<-NULL
+  }
+  get<-function() x
+  setinv<-function(inverse) invx<<-inverse
+  getinv<-function() invx
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve should 1. check for inverse of a matrix and if not it doesn't find it should 2. make inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invx <- x$getinv()
+  if (!is.null(invx)){
+    message("getting cached data")
+    return (invx)
+  }
+  data <-x$get()
+  invx<-solve(data,...)
+  x$setinv(invx)
+  invx
+  
+  ## Return a matrix that is the inverse of 'x'
 }
